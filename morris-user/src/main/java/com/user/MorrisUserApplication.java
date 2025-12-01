2cf40134884678e541520c9cd9182e7b13894ae3
mishenghuachiai <stonegkk@gmail.com>
2025-12-01 08:24:33 +0800

    first commit

---
+++ morris-user/src/main/java/com/user/MorrisUserApplication.java
@@ -0,0 +1,12 @@
+package com.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorrisUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorrisUserApplication.class, args);
+    }
+}
---
+++ morris-user/src/main/java/com/user/common/api/Result.java
@@ -0,0 +1,31 @@
+package com.user.common.api;
+
+import lombok.Data;
+import java.time.Instant;
+
+@Data
+public class Result<T> {
+
+    private int code;
+    private String message;
+    private T data;
+    private long timestamp;
+
+    public Result(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        this.timestamp = Instant.now().toEpochMilli();
+    }
+
+    public static <T> Result<T> success() {
+        return new Result<>(200, "success", null);
+    }
+    public static <T> Result<T> success(T data) {
+        return new Result<>(200, "success", data);
+    }
+
+    public static <T> Result<T> fail(int code, String message) {
+        return new Result<>(code, message, null);
+    }
+}
